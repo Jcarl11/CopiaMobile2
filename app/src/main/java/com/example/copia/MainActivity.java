fbcadf1a9f19667c2c403c9b91bb5f1666f4c60d
joey11 <joeycarlofrancisco@yahoo.com>
2019-01-23 12:53:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/copia/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.copia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.parse.Parse;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                // if defined
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build());
+    }
+}
