e71fd752d3c0cc6dc63f0eb4b937bcde69766e2a
joey11 <joeycarlofrancisco@yahoo.com>
2019-01-23 15:59:59 +0800

    Fragments Added

--- app/src/main/java/com/example/copia/MainActivity.java
+++ app/src/main/java/com/example/copia/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.copia;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.parse.Parse;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +25,57 @@ public class MainActivity extends AppCompatActivity {
                 .clientKey(getString(R.string.back4app_client_key))
                 .server(getString(R.string.back4app_server_url))
                 .build());
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        if(savedInstanceState == null)
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentWelcome()).commit();
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId())
+        {
+            case R.id.category_client:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentClient()).commit();
+                break;
+            case R.id.category_suppliers:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentSuppliers()).commit();
+                break;
+            case R.id.category_contractors:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentContractors()).commit();
+                break;
+            case R.id.category_consultants:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentConsultants()).commit();
+                break;
+            case R.id.category_specifications:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentSpecifications()).commit();
+                break;
+            case R.id.category_search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentSearch()).commit();
+                break;
+            case R.id.logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START))
+            drawer.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
     }
 }
