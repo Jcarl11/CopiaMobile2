d51c94d65d995fcf9a57db308ad52eb2ae595c38
joey11 <joeycarlofrancisco@yahoo.com>
2019-02-13 11:59:09 +0800

    added delete listener and new recyclerview library

--- app/src/main/java/com/example/copia/FragmentSearch.java
+++ app/src/main/java/com/example/copia/FragmentSearch.java
@@ -1,5 +1,7 @@
 package com.example.copia;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -22,6 +24,8 @@ import com.example.copia.Entities.ClientEntity;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.github.codefalling.recyclerviewswipedismiss.SwipeDismissRecyclerViewTouchListener;
+
 public class FragmentSearch extends Fragment
 {
     ArrayList<String> searchCategories = new ArrayList<>();
@@ -30,6 +34,8 @@ public class FragmentSearch extends Fragment
     RecyclerView search_recyclerview;
     ClientAdapter clientAdapter;
     List<ClientEntity> clientEntities;
+    boolean option;
+    int pos = -1;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -43,20 +49,19 @@ public class FragmentSearch extends Fragment
         search_recyclerview = (RecyclerView)view.findViewById(R.id.search_recyclerview);
         search_recyclerview.setHasFixedSize(true);
         search_recyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
-        clientEntities.add(new ClientEntity("Joey Francisco", "Janitor", "Mapua", "Food and Beverage", "Government"));
-        clientEntities.add(new ClientEntity("Joey Francisco", "Janitor", "Mapua", "Food and Beverage", "Government"));
-        clientEntities.add(new ClientEntity("Joey Francisco", "Janitor", "Mapua", "Food and Beverage", "Government"));
-        clientEntities.add(new ClientEntity("Joey Francisco", "Janitor", "Mapua", "Food and Beverage", "Government"));
-        clientEntities.add(new ClientEntity("Joey Francisco", "Janitor", "Mapua", "Food and Beverage", "Government"));
-        clientEntities.add(new ClientEntity("Joey Francisco", "Janitor", "Mapua", "Food and Beverage", "Government"));
-        clientEntities.add(new ClientEntity("Joey Francisco", "Janitor", "Mapua", "Food and Beverage", "Government"));
-        clientEntities.add(new ClientEntity("Joey Francisco", "Janitor", "Mapua", "Food and Beverage", "Government"));
-        clientEntities.add(new ClientEntity("Joey Francisco", "Janitor", "Mapua", "Food and Beverage", "Government"));
+        clientEntities.add(new ClientEntity("Joey", "Janitor", "Mapua", "Food and Beverage", "Government"));
+        clientEntities.add(new ClientEntity("Ramon", "Janitor", "Mapua", "Food and Beverage", "Government"));
+        clientEntities.add(new ClientEntity("Juan", "Janitor", "Mapua", "Food and Beverage", "Government"));
+        clientEntities.add(new ClientEntity("Ana", "Janitor", "Mapua", "Food and Beverage", "Government"));
+        clientEntities.add(new ClientEntity("Pedro", "Janitor", "Mapua", "Food and Beverage", "Government"));
+        clientEntities.add(new ClientEntity("John", "Janitor", "Mapua", "Food and Beverage", "Government"));
+        clientEntities.add(new ClientEntity("Mike", "Janitor", "Mapua", "Food and Beverage", "Government"));
+        clientEntities.add(new ClientEntity("Randy", "Janitor", "Mapua", "Food and Beverage", "Government"));
+        clientEntities.add(new ClientEntity("Sam", "Janitor", "Mapua", "Food and Beverage", "Government"));
         clientAdapter = new ClientAdapter(getContext(), clientEntities);
         search_recyclerview.setAdapter(clientAdapter);
-
-
-
+        option = false;
+        search_recyclerview.setOnTouchListener(listener());
         search_spinner_searchin = (Spinner)view.findViewById(R.id.search_spinner_searchin);
         search_spinner_searchin.setAdapter(dataAdapter);
         search_edittext_search = (EditText)view.findViewById(R.id.search_edittext_search);
@@ -69,5 +74,57 @@ public class FragmentSearch extends Fragment
             }
         });
         return view;
+    }
+    private SwipeDismissRecyclerViewTouchListener listener()
+    {
+        SwipeDismissRecyclerViewTouchListener listener = new SwipeDismissRecyclerViewTouchListener.Builder(
+                search_recyclerview,
+                new SwipeDismissRecyclerViewTouchListener.DismissCallbacks() {
+                    @Override
+                    public boolean canDismiss(int position) {
+                        pos = position;
+                        return true;
+                    }
+
+                    @Override
+                    public void onDismiss(View view) {
+                        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which){
+                                    case DialogInterface.BUTTON_POSITIVE:
+                                        search_recyclerview.removeView(view);
+                                        clientEntities.remove(pos);
+                                        clientAdapter.notifyItemRemoved(pos);
+                                        break;
+
+                                    case DialogInterface.BUTTON_NEGATIVE:
+                                        break;
+                                }
+                            }
+                        };
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                        builder.setMessage("This record will be deleted permanently").setPositiveButton("Yes", dialogClickListener)
+                                .setNegativeButton("No", dialogClickListener).show();
+
+                    }
+                })
+                .setIsVertical(false)
+                .setItemTouchCallback(
+                        new SwipeDismissRecyclerViewTouchListener.OnItemTouchCallBack() {
+                            @Override
+                            public void onTouch(int index) {
+                                // Do what you want when item be touched
+                            }
+                        })
+                .setItemClickCallback(new SwipeDismissRecyclerViewTouchListener.OnItemClickCallBack() {
+                    @Override
+                    public void onClick(int i) {
+                        Utilities.getInstance().showAlertBox("Touched", "Row Touched", getContext());
+                    }
+                })
+                .create();
+        return listener;
     }
 }
