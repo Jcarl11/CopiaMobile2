329463fbfbf017535159e01bce089dc23fdcede6
joey11 <joeycarlofrancisco@yahoo.com>
2019-02-13 21:54:04 +0800

    added background images

--- app/src/main/java/com/example/copia/FragmentWelcome.java
+++ app/src/main/java/com/example/copia/FragmentWelcome.java
@@ -7,12 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 public class FragmentWelcome extends Fragment
 {
+    TextView welcome_textview_username, welcome_textview_email;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+        View view = inflater.inflate(R.layout.fragment_welcome, container, false);
+        welcome_textview_username = (TextView)view.findViewById(R.id.welcome_textview_name);
+        welcome_textview_email = (TextView)view.findViewById(R.id.welcome_textview_email);
+        ParseUser user = ParseUser.getCurrentUser();
+        welcome_textview_username.setText(user.getUsername());
+        welcome_textview_email.setText(user.getEmail());
+        return view;
     }
 }
