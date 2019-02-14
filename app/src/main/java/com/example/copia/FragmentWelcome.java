33c0df954afc2a4207a860b79aef0d9152242966
joey11 <joeycarlofrancisco@yahoo.com>
2019-02-14 15:09:59 +0800

    added currently logged in in navigation header

--- app/src/main/java/com/example/copia/FragmentWelcome.java
+++ app/src/main/java/com/example/copia/FragmentWelcome.java
@@ -13,16 +13,10 @@ import com.parse.ParseUser;
 
 public class FragmentWelcome extends Fragment
 {
-    TextView welcome_textview_username, welcome_textview_email;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_welcome, container, false);
-        welcome_textview_username = (TextView)view.findViewById(R.id.welcome_textview_name);
-        welcome_textview_email = (TextView)view.findViewById(R.id.welcome_textview_email);
-        ParseUser user = ParseUser.getCurrentUser();
-        welcome_textview_username.setText(user.getUsername());
-        welcome_textview_email.setText(user.getEmail());
         return view;
     }
 }
--- app/src/main/java/com/example/copia/MainActivity.java
+++ app/src/main/java/com/example/copia/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.parse.FindCallback;
 import com.parse.Parse;
@@ -27,10 +29,12 @@ import dmax.dialog.SpotsDialog;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
+    TextView textview_username, textview_email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
                 .clientKey(getString(R.string.back4app_client_key))
@@ -46,15 +50,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             new RetrieveComboBoxTask().execute((Void) null);
         }
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
         navigationView.setNavigationItemSelectedListener(this);
         if(savedInstanceState == null)
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentWelcome()).commit();
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
-
-
+        textview_username = (TextView)headerView.findViewById(R.id.textview_username);
+        textview_email = (TextView)headerView.findViewById(R.id.textview_email);
+        textview_username.setText("Logged in as: " + user.getUsername());
+        textview_email.setText(user.getEmail());
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
